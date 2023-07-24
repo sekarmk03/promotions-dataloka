7eab4459eca23234c497a30eeba5826934e675cf
Sekar Madu Kusumawardani <sekar.kusumawardani@digits.id>
2023-07-24 09:49:12 +0700

    Feat: add server file

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT;
+process.env.TZ = "Asia/Jakarta";
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+app.use(express.urlencoded({extended: true}));
+
+app.use('/v1', router);
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        status: 'NOT FOUND',
+        message: 'Resource Not Found',
+        data: null
+    });
+});
+
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        status: 'INTERNAL SERVER ERROR',
+        message: err.message,
+        data: null
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
